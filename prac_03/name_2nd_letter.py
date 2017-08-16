9404e5640944f911e1884283bc4c9a652c8b6571
David plummer <david.plummer1@my.jcu.edu.au>
2017-08-16 18:54:00 +1000

    "Uploading practicals from weeks 2-3"

---
+++ prac_03/name_2nd_letter.py
@@ -0,0 +1,13 @@
+"""David Plummer"""
+
+def main():
+
+    def my_name():
+        name = input("Enter your Name: ")
+        while name == "":
+            print("Invalid input")
+            name = input("Enter your Name: ")
+
+        print(name[::2])
+    my_name()
+main()
